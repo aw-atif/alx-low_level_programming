2ef2fd4a7fa4e4a110ed7211d9edee0a229ea73e
Awatif YOUSFi <hello.yousfiawatif@gmail.com>
2023-06-09 07:39:06 -0700

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * That program print his name
+ * argc: number of arguments
+ * argv: array of arguments
+ * Return 0 if it success
+ * */
 int main (int argc, char *argv[]){
-    printf("%s", argv[0]);
+    printf("%s\n", argv[0]);
     return 0;
 }
