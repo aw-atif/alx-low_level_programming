8767593a44266709c1ddac9ced0c4052889622eb
awatif <hello.yousfiawatif@gmail.com>
2023-08-03 03:13:18 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,14 @@
  * @s: input
  * Return: Always 0 (Success)
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		putchar(*s);
+		_putchar(*s);
 		_puts_recursion(s + 1);
-	} else
-	putchar('\n');
+	}
+
+	else
+		_putchar('\n');
 }
